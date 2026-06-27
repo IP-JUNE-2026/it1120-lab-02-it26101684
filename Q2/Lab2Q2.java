b158506b1b4c68ca809f62933939e354ab2c365d
yumin <it26101684@my.sliit.lk>
2026-06-27 09:28:11 +0530

    Add files via upload

---
+++ Q2/Lab2Q2.java
@@ -0,0 +1,16 @@
+public class Lab2Q2 {
+
+    public static void main(String[] args) {
+
+        double length = 10;
+        double perimeter;
+        double radius;
+
+        perimeter = 4 * length;
+
+        radius = perimeter / (2 * 3.14);
+
+        System.out.println("Radius of the circular fence: " + radius);
+
+    }
+}
